0c71ce254eaded1b580713a62370e8f1352dcfed
Cedric de Crousaz <cedric.de-crousaz@hexagon.com>
2022-12-05 10:23:40 +0100

    [day05] problem setup

---
+++ day05/main.cpp
@@ -0,0 +1,55 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace {
+    enum class Part {
+        FIRST = 0,
+        SECOND
+    };
+}
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        std::cout << "Please provide input file" << std::endl;
+        return 1;
+    }
+
+    Part part = Part::FIRST;
+    if (argc > 2) {
+        unsigned int part_value = std::atol(argv[2]);
+        if (part_value == 0) {
+            part = Part::FIRST;
+        } else if (part_value == 1) {
+            part = Part::SECOND;
+        } else {
+            std::cout << "Invalid part number: " << part_value << std::endl;
+            return 1;
+        }
+    }
+
+    std::string filename = argv[1];
+    std::ifstream ifile(argv[1]);
+
+    if (!ifile.good()) {
+        std::cout << "Could not find " << filename << std::endl;
+        return 1;
+    }
+
+    switch (part) {
+        case Part::FIRST:
+        {
+            // TODO
+        }
+        break;
+        case Part::SECOND:
+        {
+            // TODO
+        }
+        break;
+    }
+
+    return 0;
+}
