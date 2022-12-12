c51cf2238701d8da404b4f48130be2cf72c510a6
Cedric de Crousaz <cedric.de-crousaz@hexagon.com>
2022-12-12 13:10:48 +0100

    [day06] part 1 & 2

--- day06/main.cpp
+++ day06/main.cpp
@@ -1,11 +1,52 @@
+#include <array>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <utility>
 
 namespace {
 enum class Part { FIRST = 0, SECOND };
-}
+
+// For high values of SIZE, having a set with a insert lookup probably more efficient
+template <std::size_t SIZE>
+struct UniqueBuffer {
+  void push(char c) {
+    if (buffer_[idx_] != c) {
+      if (contains(c)) {
+        ++duplicate_counter_;
+      }
+
+      char old_char = std::exchange(buffer_[idx_], c);
+
+      if (contains(old_char)) {
+        --duplicate_counter_;
+      }
+    }
+
+    idx_ = (idx_ + 1) % BUFFER_SIZE_;
+  }
+
+  bool isUnique() const { return (duplicate_counter_ == 0); }
+
+  bool contains(char c) {
+    for (const char other : buffer_) {
+      if (c == other) return true;
+    }
+
+    return false;
+  }
+
+ private:
+  static constexpr std::size_t BUFFER_SIZE_ = SIZE;
+  std::size_t idx_ = 0;
+  std::array<char, SIZE> buffer_ = {};  // zero-initialized
+
+  // for each duplicate character, increase by one
+  // Note : N identical characters == N-1 duplicates
+  std::size_t duplicate_counter_ = BUFFER_SIZE_ - 1;
+};
+}  // namespace
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -36,10 +77,46 @@ int main(int argc, char** argv) {
 
   switch (part) {
     case Part::FIRST: {
-      // TODO
+      static constexpr std::size_t MSG_LEN = 4;
+      std::size_t pos = 0;
+      UniqueBuffer<MSG_LEN> buffer;
+
+      bool start_found = false;
+      char c;
+      while (!start_found && ifile.get(c)) {
+        ++pos;
+        buffer.push(c);
+        if (buffer.isUnique() && pos >= MSG_LEN) {
+          start_found = true;
+        }
+      }
+
+      if (start_found) {
+        std::cout << "Start sequence ended at character " << pos << std::endl;
+      } else {
+        std::cout << "No start sequence found" << std::endl;
+      }
     } break;
     case Part::SECOND: {
-      // TODO
+      static constexpr std::size_t MSG_LEN = 14;
+      std::size_t pos = 0;
+      UniqueBuffer<MSG_LEN> buffer;
+
+      bool start_found = false;
+      char c;
+      while (!start_found && ifile.get(c)) {
+        ++pos;
+        buffer.push(c);
+        if (buffer.isUnique() && pos >= MSG_LEN) {
+          start_found = true;
+        }
+      }
+
+      if (start_found) {
+        std::cout << "Start packet sequence ended at character " << pos << std::endl;
+      } else {
+        std::cout << "No packet start found" << std::endl;
+      }
     } break;
   }
 
